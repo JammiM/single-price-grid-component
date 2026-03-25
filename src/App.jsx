7a349579e87fe767104a89e5909ee87b0a1f59e7
James Morrissey <jammim19@gmail.com>
2026-03-25 13:34:57 +0000

    Added a basic CSS Grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import SinglePriceGrid from "./components/SinglePriceGrid";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <section id="center">
+      <main id="main">
+        <SinglePriceGrid />
+      </main>
+
+      {/* <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -27,9 +32,9 @@ function App() {
         >
           Count is {count}
         </button>
-      </section>
+      </section> */}
 
-      <div className="ticks"></div>
+      {/* <div className="ticks"></div>
 
       <section id="next-steps">
         <div id="docs">
@@ -113,7 +118,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
-      <section id="spacer"></section>
+      <section id="spacer"></section> */}
     </>
   );
 }
---
+++ src/components/SinglePriceGrid.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SinglePriceGrid.css";
+
+const SinglePriceGrid = () => {
+  return (
+    <div className="SinglePriceGrid">
+      <div className="header">
+        Join our community 30-day, hassle-free money back guarantee Gain access
+        to our full library of tutorials along with expert code reviews. Perfect
+        for any developers who are serious about honing their skills.
+      </div>
+      <div className="price">
+        Monthly Subscription &#36;29 per month Full access for less than &#36;1
+        a day Sign Up
+      </div>
+      <div className="why-us">
+        Why Us Tutorials by industry experts Peer &amp; expert code review
+        Coding exercises Access to our GitHub repos Community forum Flashcard
+        decks New videos every week
+      </div>
+    </div>
+  );
+};
+
+export default SinglePriceGrid;
