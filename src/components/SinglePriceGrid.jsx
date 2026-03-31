4991c17790b10e29e816a1f20dd6f94cc295fecc
James Morrissey <jammim19@gmail.com>
2026-03-31 23:25:55 +0100

    Did some general clean up of the DOM

--- src/components/SinglePriceGrid.jsx
+++ src/components/SinglePriceGrid.jsx
@@ -3,11 +3,9 @@ import "./SinglePriceGrid.css";
 
 const SinglePriceGrid = () => {
   return (
-    <div className="SinglePriceGrid">
+    <div className="single-price-grid">
       <section className="header">
-        {/* <p className="header__title">Join our community</p> */}
         <h1 className="header__title">Join our community</h1>
-
         <h2 className="header__sub_title">
           30-day, hassle-free money back guarantee
         </h2>
@@ -17,25 +15,21 @@ const SinglePriceGrid = () => {
           skills.
         </p>
       </section>
-      <section className="sign_up">
-        {/* <p className="sign-up-heading">Monthly Subscription</p> */}
-        <h2 className="sign-up-heading">Monthly Subscription</h2>
-
-        <div class="sign-up-pricing">
-          <p className="sign-up-pricing-cost">&#36;29</p>
-          <span class="sign-up-pricing-month">per month</span>
+      <section className="sign-up">
+        <h2 className="sign-up__heading">Monthly Subscription</h2>
+        <div class="sign-up__pricing">
+          <p className="sign-up__pricing-cost">&#36;29</p>
+          <span class="sign-up__pricing-month">per month</span>
         </div>
         <p className="sign_up__description">
           Full access for less than &#36;1 a day{" "}
         </p>
-        <button type="button" className="sign_up__button">
+        <button type="button" className="sign-up__button">
           Sign Up
         </button>
       </section>
       <section className="why-us">
-        {/* <p className="why-us__heading">Why Us</p> */}
         <h2 className="why-us__heading">Why Us</h2>
-
         <p className="why-us__description">
           Tutorials by industry experts Peer &amp; expert code review Coding
           exercises Access to our GitHub repos Community forum Flashcard decks
