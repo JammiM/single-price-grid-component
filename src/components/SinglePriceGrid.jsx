20f5f683d015416f29b06ad15d3da7ed6fcbf432
James Morrissey <jammim19@gmail.com>
2026-03-31 18:38:01 +0100

    Added the general outline of the component

--- src/components/SinglePriceGrid.jsx
+++ src/components/SinglePriceGrid.jsx
@@ -5,18 +5,37 @@ const SinglePriceGrid = () => {
   return (
     <div className="SinglePriceGrid">
       <div className="header">
-        Join our community 30-day, hassle-free money back guarantee Gain access
-        to our full library of tutorials along with expert code reviews. Perfect
-        for any developers who are serious about honing their skills.
+        <p className="header__title">Join our community</p>
+        <p className="header__sub_title">
+          30-day, hassle-free money back guarantee
+        </p>
+        <p className="header__paragraph">
+          Gain access to our full library of tutorials along with expert code
+          reviews. Perfect for any developers who are serious about honing their
+          skills.
+        </p>
       </div>
-      <div className="price">
-        Monthly Subscription &#36;29 per month Full access for less than &#36;1
-        a day Sign Up
+      <div className="sign_up">
+        <p className="sign_up__heading">Monthly Subscription</p>
+        <div>
+          <p className="sign_up__month">
+            &#36;29<span className="sign_up__month--muted">per month</span>
+          </p>
+        </div>
+        <p className="sign_up__description">
+          Full access for less than &#36;1 a day{" "}
+        </p>
+        <button type="button" className="sign_up__button">
+          Sign Up
+        </button>
       </div>
       <div className="why-us">
-        Why Us Tutorials by industry experts Peer &amp; expert code review
-        Coding exercises Access to our GitHub repos Community forum Flashcard
-        decks New videos every week
+        <p className="why-us__heading">Why Us</p>
+        <p className="why-us__description">
+          Tutorials by industry experts Peer &amp; expert code review Coding
+          exercises Access to our GitHub repos Community forum Flashcard decks
+          New videos every week
+        </p>
       </div>
     </div>
   );
